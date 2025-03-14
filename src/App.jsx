c4facbbd6a21f31f62a2f7b6ad221ef759e31689
Maksim Chalyh <nazgulius@yandex.ru>
2025-03-14 22:45:26 +0300

    class OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ShopItemClass from './components/ShopItemClass'
+import Item from './components/Item'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="container">
@@ -14,7 +12,7 @@ function App() {
         <div className='highlight-overlay'></div>
       </div>
       <div className="window">
-        <ShopItemFunc item={item} />
+        <ShopItemClass item={Item} />
       </div>
     </div>
   )
